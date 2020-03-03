645a811ab6ec58b5bb61f509c2fbbc000047b8a8
Hussein W <38099907+halaeddine@users.noreply.github.com>
2020-03-03 22:00:26 +0200

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
-  
+  //asdas
   {
     path: 'dashboard',
     // canActivate: [AuthGuard],
