296a32ebb5019efaaafb19b99e434f64886648d8
Hussein W <38099907+halaeddine@users.noreply.github.com>
2020-03-16 19:39:07 +0200

    edit business pages done without images

--- src/app/my-businesses/my-businesses.page.ts
+++ src/app/my-businesses/my-businesses.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Router} from '@angular/router';
 import { HTTP } from '@ionic-native/http/ngx';
@@ -17,7 +16,6 @@ public userid:string = "";
 businessesdata:any;
 loading:any;
   constructor(
-    public navCtrl: NavController,
     public storage:Storage,
     private router: Router,
     private http: HTTP,
@@ -29,38 +27,32 @@ loading:any;
     this.getBussinesses(this.userid);
    });
 
-this.businesses = [
-// {
-//   		'id':1,
-//   		'businessName':'Hussein Barber',
-//   		'businessPhoneNumber':'70785760',
-//   		'businessDesc':'abc123',
-//   		'businessViews':'30'
-//   	},
-//   	{	'id':2,
-//   		'businessName':'Bilal Barber',
-//   		'businessPhoneNumber':'70785760',
-//   		'businessDesc':'abc1234',
-//   		'businessViews':'10'
-//   	}
-    ];
+this.businesses = [];
 
    }
 
   ngOnInit() {
   }
 
-  goToBusinessDetails(id){
-this.navCtrl.navigateForward('businessdetails/'+ id);
+  goToMyBusinessDetails(id){
+    // console.log(id);
+     this.businesses.forEach((val,key)=>{
+       // console.log(val);
+    if(val.businessId == id){
+    this.storage.set('myBusinessDetailsSelected', JSON.stringify(val));
+    this.router.navigate(['my-business-details']);
+  }
+});
+// this.navCtrl.navigateForward('businessdetails/'+ id);
 }
 
 getBussinesses(id){
   this.presentLoading();
      return new Promise((resolve, reject)=>{
-        this.http.get('http://www.brands-tech.com/api/getuserdata',{userId:id},{})
+        this.http.get('http://www.brands-tech.com/api/getuserbusinesses',{userId:id},{})
         .then(data => {
-          this.businessesdata = JSON.parse(data.data);
-          this.businesses = this.businessesdata.businesses;
+          this.businesses = JSON.parse(data.data);
+          // this.businesses = this.businessesdata.businesses;
           this.dismissLoading();
            resolve(true);
           }).catch(err=>{
